236009db742e7bab72084d10112df4b95f1333e8
Govind <bardavalgovind2005@gmail.com>
2024-10-12 12:25:36 +0530

    updated schema for user and Note

--- server/server.js
+++ server/server.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Initialize dotenv to access environment variables
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
 app.use(express.json()); // Parse JSON data
 
+// MongoDB Connection
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: process.env.DB_NAME,
+        });
+        console.log('MongoDB connection is successful.');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        
+    }
+};
+
+
+connectDb();
+
+// Example route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Notes App API');
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
