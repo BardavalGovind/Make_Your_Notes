271e7b7f4d5e51e1f50ed4fbd1214b50038d9924
Govind <bardavalgovind2005@gmail.com>
2024-12-10 18:22:59 +0530

    routes setup

--- server/index.js
+++ server/index.js
@@ -2,26 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
+const bodyParser = require('body-parser');
 
-// Initialize dotenv to access environment variables
-dotenv.config();
+const authRoutes = require("./Routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
+dotenv.config();
 app.use(cors());
-app.use(express.json()); // Parse JSON data
+app.use(express.json()); 
 
-app.use('/auth', authRoutes);
+
 
 // MongoDB Connection
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: process.env.DB_NAME,
-        });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('MongoDB connection is successful.');
     } catch (error) {
         console.error('MongoDB connection error:', error);
@@ -32,12 +29,19 @@ const connectDb = async () => {
 
 connectDb();
 
-// Example route
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Notes App API');
 });
 
-// Start the server
+app.use("/auth", authRoutes);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+// PORT=5000
+// MONGODB_URI="mongodb+srv://bardavalgovind:GnS2005@cluster1.ouyfx.mongodb.net/NewNotes?retryWrites=true&w=majority"
+// DB_NAME="NewNotes"
+// JWT_SECRET_KEY="mynotes"
