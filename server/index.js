26dc100ad1c677712eee0a652d8b6390c42335fe
Govind <bardavalgovind2005@gmail.com>
2024-12-11 12:23:06 +0530

    backend multer completed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const authRoutes = require("./routes/auth");
+const noteRoutes = require("./routes/notes")
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -36,6 +37,7 @@ app.get('/', (req, res) => {
 });
 
 app.use("/auth", authRoutes);
+app.use("/notes", noteRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
