de516b51f6d7eb8a4b64d222fd4d2e30611f4dd2
Govind <bardavalgovind2005@gmail.com>
2024-12-12 18:26:57 +0530

    about us updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import Header from './components/Header';
 import Search from './pages/Search';
+import About from './pages/About';
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>} />
-          {/* <Route path='/about' element={<Home/>} /> */}
+          <Route path='/about' element={<About/>} />
           {/* <Route path='/faq' element={<Home/>} /> */}
           {/* <Route path='/profile' element={<Home/>} /> */}
           {/* <Route path='/login' element={<Home/>} /> */}
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -23,16 +23,52 @@ const SearchBar = () => {
                  dark:bg-blue-600 dark:hover:bg-blue-700
                  dark:focus:ring-blue-800'>
                     Search</button>
-                
             </div>
         </form>
       </div>
       {/* documents */}
-      <div className='mt-5 h-full w-full border border-amber-500'>
-        aaaaaaaaaaaaaaaaaaa
+      <div className='mt-5
+       w-full grid grid-cols-1 gap-5 border
+        border-amber-500 sm:grid-cols-2 lg:grid-cols-4'>
+        {/* <div className='flex w-[290px] items-center
+        justify-between border border-black
+         bg-[#374151] px-4 py-2 text-white'>
+            <p className=''>
+                <span className='font-bold'>File Name :</span>
+                <span>{' '}Sem8</span>
+            </p>
+            <button className='rounded-xl bg-blue-500
+             px-4 py-1 font-bold hover:bg-blue-600'>
+                Show File
+            </button>
+        </div> */}
+        { Array(8)
+        .fill(true)
+        .map((item, i)=>(
+                <div
+                 key={i}
+                 className='flex w-[290px] items-center
+                justify-between border rounded-lg border-black
+                bg-[#374151] px-4 py-2 text-white'>
+                    <p className=''>
+                        <span className='font-bold'>File Name :</span>
+                        <span>{' '}Sem8</span>
+                    </p>
+                    <button className='rounded-xl bg-blue-500
+                    px-4 py-1 font-bold hover:bg-blue-600'>
+                        Show File
+                </button>
+                </div>
+        ))}
       </div>
     </div>
   );
 };
-
 export default SearchBar;
+
+
+
+
+
+
+
