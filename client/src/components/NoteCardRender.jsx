efb7af8aaa5215f941a3918f37ee8b573a49416d
Govind <bardavalgovind2005@gmail.com>
2025-01-05 14:29:11 +0530

    frontend part for note creation is completed

--- client/src/components/NoteCardRender.jsx
+++ client/src/components/NoteCardRender.jsx
@@ -57,7 +57,13 @@ const NoteCardRender = () => {
                 contentLabel="Add or Edit Notes Modal"
                 className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
             >
-                <AddEditNotes type={openAddEditModal.type} data={openAddEditModal.data} />
+                <AddEditNotes 
+                    type={openAddEditModal.type}
+                    noteData={openAddEditModal.data}
+                    onClose={()=> {
+                        setOpenAddEditModal({ isShown: false, type: "add", data: null });
+                    }}
+                 />
             </Modal>
         </>
     );
--- client/src/pages/AddEditNotes.jsx
+++ client/src/pages/AddEditNotes.jsx
@@ -1,21 +1,63 @@
 import React from 'react'
 import TagInput from '../components/TagInput';
 import { useState } from "react";
+import { MdClose } from "react-icons/md";
 
-const AddEditNotes = () => {
+
+const AddEditNotes = ({ noteData, type, onClose }) => {
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [tags, setTags] = useState([])
 
+  const [error, setError] = useState(null);
+
+  // add note
+  const addNewNote = async ()=>{
+
+  }
+
+  // edit note
+  const editNote = async ()=>{
+    
+  }
+
+  const handleAddNote = ()=>{
+    if(!title){
+      setError("please enter the title");
+      return;
+    };
+
+    if(!content){
+      setError("please enter the content")
+      return;
+    }
+
+    setError("");
+
+    if(type === 'edit'){
+      editNote();
+    }
+    else{
+      addNewNote();
+    }
+  }
   return (
-    <div>
+
+
+    <div className='relative'>
+      <button className='w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-500' onClick={onClose}>
+        <MdClose className="text-xl text-slate-400"/>
+      </button>
+
       <div className='flex flex-col gap-2'>
         <label className='input-label'>TITLE</label>
         <input
          type="text"
          className='text-2xl text-slate-950 outline-none'
          placeholder='Go To Gym At 5' 
+         value={title}
+         onChange={({ target }) => setTitle(target.value)}
          />
       </div>
 
@@ -26,6 +68,8 @@ const AddEditNotes = () => {
          className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded'
          placeholder='Content'
          rows={10}
+         value={content}
+         onChange={({ target }) => setContent(target.value)}
          ></textarea>
       </div>
 
@@ -34,8 +78,12 @@ const AddEditNotes = () => {
         <TagInput tags={tags} setTags={setTags}/>
       </div>
 
-      <button className='bg-blue-500 font-medium mt-5 p-3 my-1 w-full rounded text-sm text-white'
-       onClick={()=> {}}>
+      {error && <p className='text-red-500 text-xs pt-4'>{error}</p>}
+
+      <button
+       className='bg-blue-500 font-medium mt-5 p-3 my-1 w-full rounded text-sm text-white'
+       onClick={handleAddNote}
+      >
         ADD
       </button>
     </div>
